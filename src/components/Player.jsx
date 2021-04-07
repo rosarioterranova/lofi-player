3e0ccf2ca9b3f4d76e7e7d9f5ff1d89833b57571
Rosario Terranova <rosarioterranova91@gmail.com>
2021-04-07 11:42:53 +0200

    installed fontawesome

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,3 +1,6 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faPlay, faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons"
+
 export default function Player(){
     return <>
     <div className="player">
@@ -9,7 +12,9 @@ export default function Player(){
         </div>
 
         <div className="play-control">
-            
+            <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+            <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+            <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight} />
         </div>
     </div>
     </>
