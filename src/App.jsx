00a89ed11be53286143f10a776cdc249a93d2500
Rosario Terranova <rosarioterranova91@gmail.com>
2021-04-07 12:28:57 +0200

    added audio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss"
 import Player from "./components/Player"
 import Song from "./components/Song"
+import data from "./data.json"
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <h1>Lo Fi Player</h1>
       <Song />
       <Player />
+      <audio controls src={data[0].path} type="audio/ogg" />
     </div>
   );
 }
